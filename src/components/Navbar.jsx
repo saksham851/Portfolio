52b2ffa7fc2b08210ebd79b7da5eeda0cd20369d
Saksham <121371714+saksham851@users.noreply.github.com>
2024-08-27 22:57:44 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar({ darkMode, toggleTheme }) {
   return (
     <>
       <div
-        className={`max-w-screen-2xl container mx-auto px-4 md:px-20 h-16 shadow-md fixed top-0 left-0 right-0 z-50 ${
+        className={`max-w-screen-2xl container mx-auto px-4 md:px-20 h-42 shadow-md fixed top-0 left-0 right-0 z-50 ${
           darkMode ? "bg-gray-800 text-white" : "bg-white text-black"
         }`}
       >
